8c85168965cc00e3be4f7dc325f0e54838e91b0d
Tony DiCola <tony@tonydicola.com>
2014-06-12 00:23:50 -0700

    Update and rename IRremote.cpp to IRremote_library.cpp
    Rename library to not conflict with Arduino internal library.

--- IRremote_library.cpp
+++ IRremote_library.cpp
@@ -16,8 +16,8 @@
  * JVC and Panasonic protocol added by Kristian Lauszus (Thanks to zenwheel and other people at the original blog post)
  */
 
-#include "IRremote.h"
-#include "IRremoteInt.h"
+#include "IRremote_library.h"
+#include "IRremoteInt_library.h"
 
 // Provides ISR
 #include <avr/interrupt.h>
